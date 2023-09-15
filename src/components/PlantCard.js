7507ca5f7e0dc72ddd2f411de0c384109f8f8ec3
Ekta <edankhra@gmail.com>
2023-09-15 10:34:21 -0400

    completed

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -1,16 +1,43 @@
-import React from "react";
+import React, {useState} from "react";
 
-function PlantCard() {
+function PlantCard({plant}) {
+  const {id, name, price, image} = plant
+  const [inStock, setInStock] = useState(true)
+  const [updatedPrice, setUpdatedPrice] = useState(0)
+
+  function handleClick(){
+    setInStock(inStock => !inStock)
+  }
+
+  function handlePriceChange(e){
+   const amount = e.target.value
+   if(amount === "" || amount === null ){
+     setUpdatedPrice(" ")
+   }else{
+     setUpdatedPrice(parseFloat(e.target.value))
+   }
+  }
+  
   return (
     <li className="card">
-      <img src={"https://via.placeholder.com/400"} alt={"plant name"} />
-      <h4>{"plant name"}</h4>
-      <p>Price: {"plant price"}</p>
-      {true ? (
-        <button className="primary">In Stock</button>
+      <img src={image} alt={name} />
+      <h4>{name}</h4>
+      <p>Price: {price.toFixed(2)}</p>
+      {inStock ? (
+        <button className="primary" onClick = {handleClick}>In Stock</button>
       ) : (
-        <button>Out of Stock</button>
+        <button onClick = {handleClick}>Out of Stock</button>
       )}
+      <form> 
+        <input type="number" 
+               step="0.01" 
+               placeholder="New Price..." 
+               value={updatedPrice} 
+               onChange={handlePriceChange}
+            />
+            <button type="submit">Updete Price </button>
+        </form>
+
     </li>
   );
 }
--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -1,9 +1,12 @@
 import React from "react";
 import PlantCard from "./PlantCard";
 
-function PlantList() {
+function PlantList({plants}) {
+
+  const pList = plants.map((plant) => <PlantCard key={plant.id} plant={plant}/>)
+  console.log(pList)
   return (
-    <ul className="cards">{/* render PlantCards components in here */}</ul>
+    <ul className="cards">{pList}</ul>
   );
 }
 
